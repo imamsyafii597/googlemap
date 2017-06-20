f59eb2616c879317aa483dc011fc6796bdfb95d8
imamsyafii597 <imamsyafii597@gmail.com>
2017-06-20 17:58:50 +0700

    Add files via upload

---
+++ gmap.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title>Live Demo of Google Maps Geocoding Example with PHP</title>
+	
+	<style>
+	body{
+		font-family:arial;
+		font-size:.8em;
+	}
+	
+	input[type=text]{
+		padding:0.5em;
+		width:20em;
+	}
+	
+	input[type=submit]{
+		padding:0.4em;
+	}
+	
+	#gmap_canvas{
+		width:100%;
+		height:30em;
+	}
+	
+	#map-label,
+	#address-examples{
+		margin:1em 0;
+	}
+	</style>
+
+</head>
+<body>
+
+<?php
+if($_POST){
+ 
+    // get latitude, longitude and formatted address
+    $data_arr = geocode($_POST['address']);
+ 
+    // if able to geocode the address
+    if($data_arr){
+         
+        $latitude = $data_arr[0];
+        $longitude = $data_arr[1];
+        $formatted_address = $data_arr[2];
+                     
+    ?>
+ 
+    <!-- google map will be shown here -->
+    <div id="gmap_canvas">Loading map...</div>
+    <div id='map-label'>Map shows approximate location.</div>
+ 
+    <!-- JavaScript to show google map -->
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js"></script>    
+    <script type="text/javascript">
+        function init_map() {
+            var myOptions = {
+                zoom: 14,
+                center: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
+            marker = new google.maps.Marker({
+                map: map,
+                position: new google.maps.LatLng(<?php echo $latitude; ?>, <?php echo $longitude; ?>)
+            });
+            infowindow = new google.maps.InfoWindow({
+                content: "<?php echo $formatted_address; ?>"
+            });
+            google.maps.event.addListener(marker, "click", function () {
+                infowindow.open(map, marker);
+            });
+            infowindow.open(map, marker);
+        }
+        google.maps.event.addDomListener(window, 'load', init_map);
+    </script>
+ 
+    <?php
+ 
+    // if unable to geocode the address
+    }else{
+        echo "No map found.";
+    }
+}
+?>
+  <form action="" method="post">
+    <input type='text' name='address' placeholder='Enter any address here' />
+    <input type='submit' value='Geocode!' />
+</form>
